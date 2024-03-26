bbf091348bfb05916a91fbb0b9d0d578179af9d6
Virender Singh <virendersinghdev1@gmail.com>
2024-03-26 12:57:32 +0530

    database connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+
+dotenv.config();
 const app = express();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("MongoDb is connected")
+}).catch(err => {
+    console.log('Connection error', err);
+});
+
 
 app.listen(3000, ()=>{
     console.log("Server running on 3000");
