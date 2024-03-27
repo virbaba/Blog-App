cffd0c2626075dd7387be2358ac7480a46cfe59e
Virender Singh <virendersinghdev1@gmail.com>
2024-03-27 12:16:31 +0530

    non-admin sidebar done

---
+++ client/src/components/DashUsers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DashUsers() {
+  return (
+    <div>DashUsers</div>
+  )
+}
+
+export default DashUsers
---
+++ client/src/components/DashboardComp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DashboardComp() {
+  return (
+    <div>DashboardComp</div>
+  )
+}
+
+export default DashboardComp
