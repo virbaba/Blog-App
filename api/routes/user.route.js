21ba358e3e11bfe5879da9ac6679bb485f0794dc
Virender Singh <virendersinghdev1@gmail.com>
2024-03-28 10:03:09 +0530

    display all user list to admin done

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { updateUser, deleteUser, signout } from "../controllers/user.controller.js";
+import { updateUser, deleteUser, getUsers, signout } from "../controllers/user.controller.js";
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.put('/update/:userId', verifyToken, updateUser);
-
 router.delete('/delete/:userId', verifyToken, deleteUser);
+router.get('/getusers', verifyToken, getUsers);
 
 
 router.post("/signout", signout);
