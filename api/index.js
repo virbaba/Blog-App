059096a95e8f287b4d3210c172933f05be854dfc
Virender Singh <virendersinghdev1@gmail.com>
2024-03-29 14:39:34 +0530

    pre-deploment done

--- api/index.js
+++ api/index.js
@@ -10,7 +10,8 @@ import commentRoutes from './routes/comment.route.js';
  
 import cookieParser from 'cookie-parser';
 
-const app = express();
+import path from 'path';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -19,6 +20,9 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connection error', err);
 });
 
+const __dirname = path.resolve();
+
+const app = express();
 // to allow json data
 app.use(express.json());
 app.use(cookieParser());
@@ -31,6 +35,13 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRotues);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+
+// for deployment
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
